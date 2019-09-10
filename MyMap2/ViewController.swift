dcc39c40aa153232735815aabd64729d7c2f1b1a
hayatosann <sasano.hayato@icloud.com>
2019-09-10 16:50:34 +0900

    Set contoller

--- MyMap2/ViewController.swift
+++ MyMap2/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        inputText.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        let searchKeyword = textField.text
+        
+        print(searchKeyword!)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(searchKeyword!, completionHandler: { (placemarks:[CLPlacemark]?,error:Error?) in
+            
+            if let placemark = placemarks?[0]{
+                if let targetCoordinate = placemark.location?.coordinate{
+                    print(targetCoordinate)
+                    
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = targetCoordinate
+                    pin.title = searchKeyword
+                    
+                    self.displayMap.addAnnotation(pin)
+                    self.displayMap.region =
+                        MKCoordinateRegion(center: targetCoordinate,latitudinalMeters: 500.0,longitudinalMeters: 500.0)
+                }
+            }
+        })
+        return true
     }
 
-
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var displayMap: MKMapView!
 }
 
